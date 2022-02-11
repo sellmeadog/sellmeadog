330ade685b842a871294fb19886746c2adf43ed4
Kennie Davis <kenniejaydavis@gmail.com>
2022-02-11 09:35:15 -0700

    tweak font scaling

--- apps/kenniejaydavis/blog/gatsby-browser.js
+++ apps/kenniejaydavis/blog/gatsby-browser.js
@@ -12,6 +12,6 @@ import '@fontsource/fira-sans/700.css';
 import '@fontsource/material-icons';
 import '@fontsource/merriweather-sans/300.css';
 import '@fontsource/merriweather-sans/400.css';
-import '@fontsource/merriweather';
+import '@fontsource/merriweather/300.css';
 import '@fontsource/merriweather/700.css';
 import '@fontsource/source-serif-pro';
